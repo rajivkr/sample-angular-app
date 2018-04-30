a9bb1591cb3c446aad4a4a216a7951505dd7617b
Rajiv K Ramakrishnan <krajiv@athenahealth.com>
2018-04-30 15:44:19 +0530

    adding new changes for componnents

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,7 @@ import {Component} from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'Offline';
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Created';
+  serverName = '';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created successfully, server name is ' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event) {
+    this.serverName = event.target.value;
   }
 
 }
